2f42d2acea7f49fbb2087b77cf6e75056a569954
BestKora <developer@bestkora.com>
2024-11-28 15:42:20 +0300

    Initial Commit

---
+++ Tide Times/Tide_TimesApp.swift
@@ -0,0 +1,18 @@
+//
+//  Tide_TimesApp.swift
+//  Tide Times
+//
+//  Created by Tatiana Kornilova on 15.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct Tide_TimesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+           // ContentView1()
+        }
+    }
+}
